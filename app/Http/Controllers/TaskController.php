8c92f1c9aabc969dfe2c4b75a848ba924f2a1ae7
Laptop\Calvin <99045362@mydavinci.nl>
2019-02-22 08:58:49 +0100

    store functie toegevoegd

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,12 +10,26 @@ class TaskController extends Controller
 {
     public function index($id){
         $tasks = Lists::find($id)->tasks;
-
     	return view('tasks.index',compact('tasks'));
     }
 
-    public function createTask(){
-    	return view('tasks.create');
+    public function createTask($id){
+    	$tasks = Lists::find($id)->tasks;
+    	return view('tasks.create',compact('tasks'));
+    }
+
+    public function storeTask(Request $request, $id){
+    	$tasks = Lists::find($id)->tasks;
+
+    	$task = new Task();
+    	$task->title = $request->title;
+    	$task->body = $request->body;
+    	$task->status = $request->status;
+    	$task->duur = $request->duur;
+    	$task->lists_id = $task->lists->id;
+    	$task->save();
+
+    	return redirect('tasks.index',compact('tasks'));
     }
 
 }
